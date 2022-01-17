bc286b0cb89bf1d42fc3873dbb6e4e3971cfa88a
John Rihacek <jrihacek@gmail.com>
2022-01-17 16:10:19 -0500

    new visual studio solution

---
+++ SuperHero/SuperHero/Server/Controllers/HeroController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHero.Shared;
+
+namespace SuperHero.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroController : ControllerBase
+    {
+        //no database, so use a list for now
+        static List<Comic> comics = new List<Comic>
+        {
+            new Comic { Name = "Marvel" },
+            new Comic { Name = "DC"}
+        };
+
+        List<Hero> heroes = new List<Hero>
+        {
+            new Hero { FirstName = "Peter", LastName = "Parker", HeroName = "Spiderman", Comic = comics[0] },
+            new Hero { FirstName = "Bruce", LastName = "Wayne", HeroName = "Batman", Comic = comics[1] }
+        };
+
+        public async Task<IActionResult> GetHeroes()
+        {
+            return Ok(heroes);
+        }
+    }
+}
---
+++ SuperHero/SuperHero/Shared/Hero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHero.Shared
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string HeroName { get; set; }
+        public Comic Comic { get; set; }
+    }
+}
